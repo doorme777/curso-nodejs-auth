9b109548368bdc6a523d96503cc74513682e91b3
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-05-03 11:32:56 -0600

    Enviar emails con nodemailer

--- utils/auth/strategies/local.strategy.js
+++ utils/auth/strategies/local.strategy.js
@@ -1,26 +1,21 @@
-const boom = require('@hapi/boom');
-const bcrypt = require('bcrypt');
 const { Strategy } = require('passport-local');
-const UserService = require('../../../services/user.service');
-const service = new UserService();
 
-const LocalStrategy = new Strategy(async (email, password, done) => {
-  try {
-    const user = await service.findByEmail(email);
-    const isMatch = user
-      ? await bcrypt.compare(password, user.password)
-      : false;
-    if (!user) {
-      return done(boom.unauthorized(), false);
+const AuthService = require('./../../../services/auth.service');
+const service = new AuthService();
+
+const LocalStrategy = new Strategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+  },
+  async (email, password, done) => {
+    try {
+      const user = await service.getUser(email, password);
+      done(null, user);
+    } catch (error) {
+      done(error, false);
     }
-    if (!(await isMatch)) {
-      return done(boom.unauthorized(), false);
-    }
-    delete user.password;
-    return done(null, user);
-  } catch (error) {
-    return done(error);
   }
-});
+);
 
 module.exports = LocalStrategy;
